9d7447f7cbe2c0a5a27b1147ac144692978ac705
Chris Hung <yinye1986@gmail.com>
2024-06-20 16:43:29 +0800

    bak

--- ftplugin/lua/conf.lua
+++ ftplugin/lua/conf.lua
@@ -1,10 +1,13 @@
-vim.api.nvim_create_user_command("StartLs",
+vim.api.nvim_create_user_command("LSStart",
     function()
         vim.lsp.start({
-            name ='lua-language-server',
-            cmd = { 'lua-language-server' },
-            
+            name = "lua-language-server",
+            cmd = { "lua-language-server" },
             root_dir = vim.fs.dirname(vim.fs.find({ 'init.lua', "" }, { upward = true })[1]),
         })
     end, {}
 )
+
+--- keymap ---
+vim.keymap.set({ "n", "v" }, ",s", ":LSStart<CR>")
+vim.keymap.set({ "n", "v" }, ",f", ":lua vim.lsp.buf.format()<CR>")
